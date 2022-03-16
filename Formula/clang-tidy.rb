6eed4eefdd1c264e9c861b4d45a8a2d43371f575
Alexander Höreth <a.hoereth@gmail.com>
2022-03-16 14:07:30 +0100

    Please remove default template comments

--- Formula/clang-tidy.rb
+++ Formula/clang-tidy.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class ClangTidy < Formula
   desc "A clang-based C++ linter tool"
   homepage "https://clang.llvm.org/docs/ClangFormat.html"
