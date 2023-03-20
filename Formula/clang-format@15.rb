24fa00cc5192894a38d8c5e5aaba041161315eb8
Alexander Höreth <a.hoereth@gmail.com>
2023-03-20 17:54:45 +0100

    Closer to 'brew extract --version 15 clang-format psiori/psiori'

--- Formula/clang-format@15.rb
+++ Formula/clang-format@15.rb
@@ -23,25 +23,15 @@ class ClangFormatAT15 < Formula
 
   livecheck do
     url :stable
-    regex(%r{href=.*?/tag/llvmorg[._-]v?(\d+(?:\.\d+)+)}i)
     strategy :github_latest
+    regex(%r{href=.*?/tag/llvmorg[._-]v?(\d+(?:\.\d+)+)}i)
   end
 
-  bottle do
-    sha256 cellar: :any_skip_relocation, arm64_ventura:  "f272332a2b75d4b2e6a51d4832a848a598b923b4c6dc14568f004ec1f8fda6a5"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "dc12db5146de47d0073253d8f2403dd89977901b1b43900592807849bdbbce4a"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "8bafecdcd368ae3efafef3ccfc94af76c0a6af5978c082cd883d40e7a98779c4"
-    sha256 cellar: :any_skip_relocation, ventura:        "86374598d776c1bff7b7d4a629cf7a498f234015a26a8c17878023e701ab3ae2"
-    sha256 cellar: :any_skip_relocation, monterey:       "a05743c7c240393d43f146fd2e6e61a1d3e5d79723eb73c6aba550d785fdc8eb"
-    sha256 cellar: :any_skip_relocation, big_sur:        "2e98e7315aaaea570c1b754ce8830b6ef914f9da4adb989adacaa8e3d2736248"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "d0e6e2b1d5bc61e7da4c34ec6a4ca7c0caae691b53a175f095f72dc1efe6318c"
-  end
+  depends_on "cmake" => :build
 
-  depends_on "cmake" => [:build, :test]
-
-  uses_from_macos "python" => [:build, :test], since: :catalina
   uses_from_macos "libxml2"
   uses_from_macos "ncurses"
+  uses_from_macos "python", since: :catalina
   uses_from_macos "zlib"
 
   on_linux do
